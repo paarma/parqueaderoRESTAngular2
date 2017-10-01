e5bf2865ae550571cc3116c244cf3fd93b4410d2
pablo <paarma>
2017-09-30 22:19:44 -0500

    Se agrega proyecto base REST

---
+++ backEnd/parqueaderoREST/src/main/java/com/co/developer/parqueaderoREST/ParqueaderoRestApplication.java
@@ -0,0 +1,12 @@
+package com.co.developer.parqueaderoREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoRestApplication.class, args);
+	}
+}
